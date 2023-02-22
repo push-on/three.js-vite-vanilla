d0f9fb49ad5eda8cad4c55dba120ca3c2cc0e6e1
imran hossain <mh.imranhossain@gmail.com>
2023-02-22 09:41:33 +0600

    Update 02/22/2023 09:41:33

--- src/lib/RedCube.ts
+++ src/lib/RedCube.ts
@@ -1,20 +1,14 @@
 import * as THREE from 'three'
 
 // sizes
-const sizes = {
-	width: 800,
-	height: 600
-}
+const sizes = { width: 800, height: 600 }
 
 // scene
 const scene = new THREE.Scene()
 
 // Object
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-const cubeMaterial = new THREE.MeshBasicMaterial({
-	color: '#c97c3c'
-})
-
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: '#c97c3c' })
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
 cubeMesh.rotateY(15)
 scene.add(cubeMesh)
@@ -24,9 +18,8 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
-
 // Render
-const canvas = document.querySelector<HTMLCanvasElement>('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl')
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true } as any)
 
 renderer.setSize(sizes.width, sizes.height)
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
 import './style.css'
 import { redCube } from './lib/RedCube'
-
 redCube
