e6481b018f729bd8d69f10a39d55223567c7a094
imran hossain <mh.imranhossain@gmail.com>
2023-02-22 13:21:44 +0600

    add

--- src/lib/RedCube.ts
+++ src/lib/RedCube.ts
@@ -1,27 +1,27 @@
 import * as THREE from 'three'
 
-// sizes
+// scene sizes
 const sizes = { width: 800, height: 600 }
 
-// scene
+// initialize scene
 const scene = new THREE.Scene()
 
-// Object
+// Object properties
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: '#c97c3c' })
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: '#f76394' })
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
 scene.add(cubeMesh)
 
-// Camera
+// Camera properties
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 scene.add(camera)
+camera.position.set(1, 1, 3)
+// cubeMesh.rotation.set(0.5, 1, 0)
 
 // position
-camera.position.set(0, 0.4, 3)
 cubeMesh.position.set(0, 0, 0)
 
 // rotation
-cubeMesh.rotation.set(0.5, 1, 0)
 camera.rotation.set(0, 0, 0)
 
 // scale
@@ -29,19 +29,35 @@ cubeMesh.scale.set(2, 0.5, 2)
 
 // distance from scene center to cubeMesh
 const cube_to_scene_center = cubeMesh.position.length()
+console.log({ cube_to_scene_center })
+
 // distance from cubeMesh to camera
 const cube_to_camera = cubeMesh.position.distanceTo(camera.position)
-
-console.log({ cube_to_scene_center })
 console.log({ cube_to_camera })
 
 // Axes Helper
 const axesHelper = new THREE.AxesHelper(3)
 scene.add(axesHelper)
 
-// Render
+// look at object
+camera.lookAt(cubeMesh.position)
 
+// re-order
+// cubeMesh.rotation.reorder("XYZ")
+
+// Object Groups
+
+
+
+
+
+
+
+
+// Render
 const canvas = document.querySelector('canvas.webgl')
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true } as any)
 renderer.setSize(sizes.width, sizes.height)
+
 export const redCube = renderer.render(scene, camera)
+
