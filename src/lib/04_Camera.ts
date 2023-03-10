52910450f1f521d087dae6168adc27a0b6a395a6
mhimranhossain <mh.imranhossain@outlook.com>
2023-03-10 14:03:39 +0600

    Update 03/10/2023 14:03:39

--- src/lib/04_Camera.ts
+++ src/lib/04_Camera.ts
@@ -1,24 +1,4 @@
-/*
-types of cameras {perspective,orthographic } 
-camera properties
-creating a custom camera control using mouse
-rotate around an object / point in space
-using threejs builtin adon / preset to control a camera
-*/
-
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-
-const sizes = { width: 600, height: 600 }
-const AR = sizes.width / sizes.height
-
-// Get mouse position
-// const cursor = { x: 0, y: 0 }
-// window.addEventListener("mousemove", (event) => {
-// 	cursor.x = event.clientX / sizes.width - 0.5
-// 	cursor.y = - (event.clientY / sizes.height - 0.5)
-// 	console.log(cursor)
-// })
 
 const scene = new THREE.Scene()
 const cubeMesh = new THREE.Mesh(
@@ -28,61 +8,45 @@ const cubeMesh = new THREE.Mesh(
 scene.add(cubeMesh)
 
 // Cameras
-
+const sizes = { width: 600, height: 600 }
+const AR = sizes.width / sizes.height
 // clipping
 const clip = { near: 0.1, far: 100 }
-
 // Perspective Camera
 const camera = new THREE.PerspectiveCamera(40, AR, clip.near, clip.far)
-
 // Orthographic camera
-// const OC = {
-// 	Left: -1 * AR,
-// 	Right: 1 * AR,
-// 	Top: 1,
-// 	Bottom: -1,
-// }
+// const OC = { Left: -1 * AR, Right: 1 * AR, Top: 1, Bottom: -1, }
 // const camera = new THREE.OrthographicCamera(OC.Left, OC.Right, OC.Top, OC.Bottom, clip.near, clip.far)
 
 const i = 5
-const c = { x: i, y: i, z: i }
-// const c = { x: 0, y: 2, z: i }
-camera.position.set(c.x, c.y, c.z)
-
+camera.position.set(i, i, i)
 camera.lookAt(new THREE.Vector3())
 scene.add(camera)
-
-
-
 scene.add(new THREE.AxesHelper(3))
 scene.add(new THREE.GridHelper(20, 30, 0x3c4043, 0x202020))
 const canvas = document.querySelector('canvas.webgl')
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true } as any)
 renderer.setSize(sizes.width, sizes.height)
 
-// const camera_distence = 10
-// const speed = 20
+const camera_distence = 10
+const speed = 20
 
 // contorls
-const contorls = new OrbitControls(camera, canvas as any)
-contorls.enableDamping = true
+const cursor = { x: 0, y: 0 }
+// get mouse position
+window.addEventListener("mousemove", (mouseEvent) => {
+	cursor.x = mouseEvent.clientX / sizes.width - 0.5
+	cursor.y = - (mouseEvent.clientY / sizes.height - 0.5)
+	console.log(cursor)
+})
 
-// const clock = new THREE.Clock()
 export const AnimationLoop = () => {
-	// const elapsedTime = clock.getElapsedTime()
-	// cubeMesh.rotation.y = elapsedTime * 0.5
 
 	// update camera
-	// camera.position.x = cursor.x * speed
-	// camera.position.y = cursor.y * speed
-
-	// camera.position.x = Math.sin(cursor.x * Math.PI * 2) * camera_distence
-	// camera.position.z = Math.cos(cursor.x * Math.PI * 2) * camera_distence
-	// camera.position.y = cursor.y * speed
-
-	// camera.lookAt(new THREE.Vector3())
-
-	contorls.update()
+	camera.position.x = Math.sin(cursor.x * Math.PI * 2) * camera_distence
+	camera.position.z = Math.cos(cursor.x * Math.PI * 2) * camera_distence
+	camera.position.y = cursor.y * speed
+	camera.lookAt(new THREE.Vector3())
 
 	renderer.render(scene, camera)
 	window.requestAnimationFrame(AnimationLoop)
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import './style.css'
 // import { AnimationLoop } from './lib/02_BasicAnimation'
 // import { AnimationLoop as GSAP } from './lib/03_GSAP'
 // import { AnimationLoop as Camera } from './lib/04_Camera'
-import { AnimationLoop as FullScreen } from './lib/05_FullScreen'
+// import { AnimationLoop as FullScreen } from './lib/05_FullScreen'
+import { AnimationLoop as Geometry } from './lib/06_Geometry'
 
 // redCube
 // AnimationLoop()
 // GSAP()
 // Camera()
-FullScreen()
+// FullScreen()
+Geometry
